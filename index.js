b6fc1569b3a3894ba43a3b667a69a1b8e49226e3
Jaxson R <life.is.cringy@gmail.com>
2021-10-23 19:41:17 -0500

    dgf

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const {Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const prefix = "?";
 
-client.on('ready', () => {
-    console.log(`Logged in!`);
+Client.on('ready', () => {
+    console.log(`Logged in!`); 
 })
 
-client.on('message', async message => {
+Client.on('message', async message => {
     if(message.content.startsWith(`{prefix}test`)){
         message.channel.send("You gay");
     }
